c6f195be7ae4caaf7ea30af32a374370cc5eba17
guilhermefn1011 <guilhermefn1011@gmail.com>
2023-02-07 16:40:55 -0300

    Rep erefomulation

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/guilhermefn1011/Dio/main/JavaScript/Portfolio/data/profile.json";
+    const url = "https://raw.githubusercontent.com/guilhermefn1011/js-development-portfolio/main/data/profile.json";
     const fetching = await fetch(url)
     return await fetching.json()
 }
